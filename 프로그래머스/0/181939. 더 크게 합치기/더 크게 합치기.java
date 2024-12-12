17d4e5bc2b92e413dd1182a3fe46b6ffd57977d0
JeongHwa <jeonghwa485@gmail.com>
2024-12-13 00:13:09 +0900

    [level 0] Title: 더 크게 합치기, Time: 1.73 ms, Memory: 82.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int a, int b) {
+        String ab = Integer.toString(a) + Integer.toString(b);
+        String ba = Integer.toString(b) + Integer.toString(a);
+        
+        if(Integer.parseInt(ab) >= Integer.parseInt(ba)){
+            return Integer.parseInt(ab); 
+        } else {
+            return Integer.parseInt(ba);
+        }
+    }
+}
