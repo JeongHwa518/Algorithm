5beaa64efc8b6e1891666700c41a89294f73a801
JeongHwa <jeonghwa485@gmail.com>
2024-12-16 00:57:09 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.54 ms, Memory: 84.9 MB -BaekjoonHub

--- 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -1,7 +1,7 @@
 class Solution {
     public int solution(String my_string) {
         int answer = 0;
-        my_string = my_string.replaceAll("[a-zA-Z]","");
+        my_string = my_string.replaceAll("[^0-9]","");
         String[] arr = my_string.split("");
         for(String a : arr) {
             answer += Integer.parseInt(a);
