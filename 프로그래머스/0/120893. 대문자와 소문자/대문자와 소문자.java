7f500e5673484938590e52e2fcd621326d9db9c6
JeongHwa <jeonghwa485@gmail.com>
2024-12-16 23:44:55 +0900

    [level 0] Title: 대문자와 소문자, Time: 12.29 ms, Memory: 89.6 MB -BaekjoonHub

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -1,13 +1,21 @@
 class Solution {
     public String solution(String my_string) {
         String answer = "";
+         char[] arr = my_string.toCharArray();
+		
+		// a ~ z 97 ~122
+		// A ~ Z 65~90
+        
+        String temp ="";
         for(int i = 0; i < my_string.length(); i++) {
-            if(Character.isUpperCase(my_string.charAt(i))) {
-                answer += Character.toLowerCase(my_string.charAt(i));
-            } else {
-                answer += Character.toUpperCase(my_string.charAt(i));
+            if(97 <= arr[i] && arr[i] <= 122) {
+                temp = arr[i] + "";
+				answer += temp.toUpperCase();
+            } else if(65 <= arr[i] && arr[i] <= 90) {
+                temp = arr[i] + "";
+				answer += temp.toLowerCase();
             }
-        }
+        }       
         return answer;
     }
 }
