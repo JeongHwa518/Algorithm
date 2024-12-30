edf9b63f502ad06276710a2daf8efe9bd4c3c853
JeongHwa <jeonghwa485@gmail.com>
2024-12-30 23:34:03 +0900

    [level 0] Title: 이어 붙인 수, Time: 1.89 ms, Memory: 86.1 MB -BaekjoonHub

--- 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.java
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.java
@@ -1,15 +1,18 @@
 class Solution {
     public int solution(int[] num_list) {
+        int answer = 0;
         String even = "";
         String odd = "";
         
-        for(int num : num_list){
-            if(num % 2 == 0){
-                even += num + "";
+        for(int i = 0; i < num_list.length; i++){
+            if(num_list[i] % 2 == 0){
+                even += String.valueOf(num_list[i]);
             } else {
-                odd += num + "";
+                odd += String.valueOf(num_list[i]);
             }
-        }        
-        return Integer.parseInt(even) + Integer.parseInt(odd);
+        }
+        answer = Integer.parseInt(even) + Integer.parseInt(odd);
+        
+        return answer;
     }
 }
