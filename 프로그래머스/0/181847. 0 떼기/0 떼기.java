b90721e1f74ab651b58544739b3057d95b21917a
JeongHwa <jeonghwa485@gmail.com>
2025-01-10 23:59:51 +0900

    [level 0] Title: 0 떼기, Time: 10.06 ms, Memory: 89.1 MB -BaekjoonHub

--- 프로그래머스/0/181847. 0 떼기/0 떼기.java
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -1,7 +1,5 @@
 class Solution {
     public String solution(String n_str) {
-        int i = Integer.parseInt(n_str);
-        String answer = i+"";
-        return answer;
+        return ""+Integer.parseInt(n_str);
     }
 }
